ee130bd8b84041acfbb25a6569eb5a6d3bf91524
jill <krakaka088@gmail.com>
2020-04-18 20:46:48 +0300

    massive overhaul, changed absolutely everything

---
+++ main.js
@@ -0,0 +1,22 @@
+function copyDiscord() {
+  let copyText = document.getElementById("discordclipboard");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  document.execCommand("copy");
+  
+  // incase that failed
+  
+  if (navigator.clipboard) {
+  	navigator.clipboard.writeText(copyText.value)
+  }
+  
+  let tooltip = document.getElementById("tag");
+  tooltip.innerHTML = "copied to clipboard";
+}
+
+function mouseOut() {
+  let tooltip = document.getElementById("tag");
+  tooltip.innerHTML = "oatmealine#1704";
+}
---
+++ sketch.js
@@ -0,0 +1,34 @@
+let awsmShader;
+
+let video;
+
+function preload(){
+  awsmShader = loadShader('effect.vert', 'effect.frag');
+  video = createVideo('assets/gameplay.mp4');
+}
+
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+  
+  video.elt.muted = true;
+  video.autoplay(true);
+  video.loop();
+  video.get();
+  video.hide();
+}
+
+function draw() {
+  shader(awsmShader);
+  awsmShader.setUniform('sampler0', video);
+  awsmShader.setUniform('time', frameCount * 0.01);
+  
+  awsmShader.setUniform('amount', 0.2);
+
+  rect(0, 0, width, height);
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
